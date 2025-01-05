5d83d1be65f9f7e5ca171cf789e9585ab130643d
sushanksinha <sushanksinha.5252@gmail.com>
2025-01-06 00:52:16 +0530

    Update cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(cors({
-    origin : ["http://localhost:5173"],
+    origin : ["http://localhost:5173", "https://ss-ems.netlify.app"],
     methods : ["GET", "POST", "PUT", "HEAD", "DELETE"],
     credentials : true
 }))
