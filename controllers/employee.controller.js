c719dcbba4dc713d5eed6dd2664710c05113f4c6
sushanksinha <sushanksinha.5252@gmail.com>
2025-07-29 04:27:26 +0530

    update cookie logic

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -49,7 +49,7 @@ export const loginEmployee = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 60 * 60 * 1000, // 1 hour
     });
 
